ba529db1b7ee3ed208fc77eecde91e1aca13189e
alice-mu <alicemu99@gmail.com>
2019-01-31 10:07:11 +0800

    Contours and rectangles all drawn; yet to figure out hierarchy so contours don't include inner circles; still errors on the last return 0 handler

--- Segment/Segment.cpp
+++ Segment/Segment.cpp
@@ -15,15 +15,11 @@ using namespace cv::ml;
 
 int main()
 {
-	//Mat img = imread("C:/Users/lenovo/Documents/Visual_Studio_Community/Data/handwriting_crop.jpg");
-	Mat img = imread("C:/Users/lenovo/Documents/Visual_Studio_Community/Data/dot3.jpg");
-
+	Mat img = imread("C:/Users/lenovo/Documents/Visual_Studio_Community/Data/handwriting_crop.jpg");
 	Mat gray;
 	cvtColor(img, gray, CV_BGR2GRAY);
 
 	threshold(gray, gray, 170, 255, THRESH_BINARY_INV);
-	//Canny(img, img, 30, 128, 3, false);
-    //adaptiveThreshold(img, img, 180, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 5, 0);
 
 	vector<Mat> contours;
 	Mat gray2;
@@ -55,43 +51,36 @@ int main()
 
 
 
-
-
-	vector<vector<Point> > contours_poly(2);
-	vector<Rect> boundRect(2);
+	vector<Mat> contours_poly(contours.size());
+	vector<Rect> boundRect(contours.size());
 	for (int i = 0; i < contours.size(); i++)
 	{
-		//approxPolyDP(contours.at(i), contours_poly[i], 3, true);
-		//boundRect[i] = boundingRect(contours.at(i));
+		approxPolyDP(contours.at(i), contours_poly[i], 3, true);
+		boundRect[i] = boundingRect(contours.at(i));
 
 		//vector<Point> curContour = contours.at(i);
-		//
-		//for (int j = 0; j < curContour.size(); j++)
+		//for (int j = 0; j < curContour.size(); j++){
 		//	cout << curContour[j].x << "  " << curContour[j].y << endl;
-
-		//cout << contours[i] << endl;
+		// }
 	}
 
 	RNG rng(12345);
 
 
 	Mat drawing = Mat::zeros(gray.size(), CV_8UC3);
+	Mat img2;
+	img.copyTo(img2);
 	for (int i = 0; i < contours.size(); i++)
 	{
 		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-		drawContours(img, contours, i, color, 1, 8, vector<Vec4i>(), 0, Point());
-		//cout << boundRect[i].tl() << endl;
-		//cout << boundRect[i].br() << endl;
-
-		rectangle(img, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0);
+		drawContours(img2, contours, i, color, 2, 8, vector<Vec4i>(), 0, Point());
+		rectangle(img2, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0);
 	}
 
 	namedWindow("Contours", CV_WINDOW_AUTOSIZE);
-	imshow("Contours", img);
-
-	//Size size(1000, 1000);
-	//resize(gray, gray, size);
-	//imshow("test", gray);
+	Size size(1500, 750);
+	resize(img2, img2, size);
+	imshow("Contours", img2);
 
 	waitKey();
 	return 0;
